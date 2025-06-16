022bd3850118a3d4246087449c044b1dcb5a9b7b
viniciochaves <viniciochavess@gmail.com>
2025-06-16 08:55:21 -0300

    style: identação e formatação

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
-export default function Home(){
-    return (
-        <>
-            <p>Bem vindo à page Home</p>
-        </>
-    )
+export default function Home() {
+  return (
+    <>
+      <p>Bem vindo à page Home</p>
+    </>
+  );
 }
